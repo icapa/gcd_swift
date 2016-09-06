730d5b3aad7298701dc75a1878f1032e137dcc14
icapa <ivan.olea@gmail.com>
2016-09-06 21:02:55 +0200

    appDidFinishLaunching

--- GCD/AppDelegate.swift
+++ GCD/AppDelegate.swift
@@ -16,6 +16,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        // Creamos la window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        // Creamos el VC
+        let mainVC = GCDViewController(nibName: nil, bundle: nil)
+        
+        // le endosamos el rootvc
+        window?.rootViewController = mainVC
+        
+        window?.makeKeyAndVisible()
+        
+        
+        
+        
         return true
     }
 
